36f2fc33eacbfba415f410af13bdb1eea3da7f33
teisei <teisei.din@gmail.com>
2014-07-15 15:23:54 +0800

    first commit

---
+++ test/src/test/test.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by dingcheng on 2014/7/15.
+ */
+public class test {
+    public static void main(String args[]){
+        System.out.println("this is a test class file!");
+    }
+}
